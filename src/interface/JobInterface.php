5bbf3268dfd3981184f6467ec2810262aa6c48f5
misaiyar <1546896791@qq.com>
2018-02-13 15:33:52 +0800

    Create JobInterface.php

---
+++ src/interface/JobInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+interface JobInterface{
+    public function popJob($queue);
+    public function pushJob($queue,$data);
+    public function fininshJob($jobid);
+    public function failedJob($jobid);
+}
+?>
